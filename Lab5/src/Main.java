694cb6d87ec52e3d1b6d9cae2f8af860e9ed1177
volguzov <117124526+volguzov@users.noreply.github.com>
2023-12-02 04:50:45 +0300

    Add files via upload

---
+++ Lab5/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Проверка, есть ли аргументы командной строки
+        if (args.length == 0) {
+            System.out.println("Нет аргументов командной строки.");
+        } else {
+            System.out.println("Аргументы командной строки:");
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Аргумент " + (i + 1) + ": " + args[i]);
+            }
+        }
+    }
+}
